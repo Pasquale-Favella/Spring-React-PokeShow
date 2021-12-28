a58979feaae724853fbd922d756f2d4dedf525b9
Favella Pasquale <FavellaPa@valueteam.com>
2021-12-28 14:04:41 +0100

    added repository stereotype

--- src/main/java/com/pokedex/repository/PokemonAbilityRepo.java
+++ src/main/java/com/pokedex/repository/PokemonAbilityRepo.java
@@ -5,7 +5,9 @@ import java.util.List;
 import com.pokedex.models.PokemonAbility;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PokemonAbilityRepo extends JpaRepository<PokemonAbility,Integer>{
 
     List<PokemonAbility> findByPokedexNum(Integer pokedexNum);
--- src/main/java/com/pokedex/repository/PokemonRepository.java
+++ src/main/java/com/pokedex/repository/PokemonRepository.java
@@ -11,7 +11,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PokemonRepository extends JpaRepository<Pokemon,Integer> {
 
     @Query(
