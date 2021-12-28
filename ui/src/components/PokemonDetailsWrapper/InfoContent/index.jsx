340fb51d4482f81c210b0275754c1d7c840e8b7a
Favella Pasquale <FavellaPa@valueteam.com>
2021-12-28 17:55:24 +0100

    right details layout

--- ui/src/components/PokemonDetailsWrapper/InfoContent/index.jsx
+++ ui/src/components/PokemonDetailsWrapper/InfoContent/index.jsx
@@ -11,9 +11,17 @@ export const InfoContent = ()=>{
 
     return (
         <div className='info-content'>
-            
+
              <PokemonImage pokemon={pokemonDetails} injectClass="info-card"/>
-             
+
+             <div className='ability-section'>
+                 <h4>Ability</h4>
+                 <ul className='ability-list'>
+                {
+                    pokemonDetails.ability.map((abilityObj , i)=><li key={i}>{abilityObj.ability}</li>)
+                }
+                </ul>
+             </div>
         </div>
     )
 }
--- ui/src/components/PokemonDetailsWrapper/SectionContent/index.jsx
+++ ui/src/components/PokemonDetailsWrapper/SectionContent/index.jsx
@@ -1,17 +1,20 @@
+import './SectionContent.scss';
+
 import { useContext } from "react";
 import PokemonDetailsContext from "../../../context/PokemonDetailContext";
 import { formatPokedexNumber } from "../../../utils";
+import { BsStars } from 'react-icons/bs';
 
 export const SectionContent = ()=>{
 
     const { pokemonDetails } = useContext(PokemonDetailsContext);
 
-    
-
     return (
-        <div>
-            <h1>{pokemonDetails.name}</h1>
-            <small>{formatPokedexNumber(pokemonDetails.pokedexNumber)}</small>
+        <div className='section-content'>
+            <div className='section-title'>
+                <h1>{pokemonDetails.name}</h1>
+                <small>{formatPokedexNumber(pokemonDetails.pokedexNumber)} {!!pokemonDetails.isLegendary && <BsStars/>}</small>
+            </div>
         </div>
     )
 }
