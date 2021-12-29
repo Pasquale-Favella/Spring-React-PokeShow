6823f3d5414e4d7273568bde6382e4ffeb71952a
Favella Pasquale <FavellaPa@valueteam.com>
2021-12-29 16:53:56 +0100

    basic stats added

--- ui/src/components/PokemonDetailsWrapper/SectionContent/index.jsx
+++ ui/src/components/PokemonDetailsWrapper/SectionContent/index.jsx
@@ -11,7 +11,22 @@ export const SectionContent = ()=>{
     return (
         <div className='section-content'>
         
-            <h2>Infos</h2>
+            <h2>Info</h2>
+
+            <div className="stats-section">
+
+                        <h4>Stats</h4>
+                        <div className="stats-table">
+
+                            <div className='stat'>Hp <span className='value'>{pokemonDetails.hp}</span></div>
+                            <div className='stat'>Attack <span className='value'>{pokemonDetails.attack}</span></div>
+                            <div className='stat'>Defense <span className='value'>{pokemonDetails.defense}</span></div>
+                            <div className='stat'>Sp.Attack <span className='value'>{pokemonDetails.spAttack}</span></div>
+                            <div className='stat'>Sp.Defense <span className='value'>{pokemonDetails.spDefense}</span></div>
+                            <div className='stat'>Speed <span className='value'>{pokemonDetails.speed}</span></div>
+                            <div className='stat'><b>Total</b> <span className='value'>{pokemonDetails.baseTotal}</span></div>
+                        </div>
+            </div>
         </div>
     )
 }
